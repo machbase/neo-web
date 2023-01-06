03a208d7014a6326d2804b467915cdd5770da1cd
CMCGTVNgoc <tongocsam2409@gmail.com>
2023-01-06 15:52:20 +0700

    .

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-    <router-view name="Header" />
-    <router-view />
-    <router-view name="Footer" />
+    <div class="dark">
+        <div class="app">
+            <router-view name="Header" />
+            <router-view />
+            <router-view name="Footer" />
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts" name="App"></script>
+<style lang="scss">
+@import './index.scss';
+</style>
--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>header</div>
+    <div class="header">header</div>
 </template>
 
 <script setup lang="ts" name="Header"></script>
