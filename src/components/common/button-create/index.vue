eed270884840481a13e4cdbfad5f220f40fc98e9
peter <taphuong.cmc@gmail.com>
2023-01-10 13:48:17 +0700

    rebasing

--- src/components/common/button-create/index.vue
+++ src/components/common/button-create/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div @click="props.onClick" class="button-wrapper" :style="{ height: props.isAddChart ? '300px' : '63px' }">
-        <img class="img-wrapper" :style="{ height: props.isAddChart ? '100px' : '32px' }" src="../../../assets/img/i_plus_off.png" />
+        <img class="img-wrapper" :style="{ height: props.isAddChart ? '100px' : '32px' }" src="../../../assets/image/i_plus_off.png" />
         <div :style="{ fontSize: props.isAddChart ? '24px' : '16px' }">
             <span class="normal-text">Click </span>
             <span class="plus-text">+ button</span>
