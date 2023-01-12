4c8957f294ecd537961e9356f504c2e6549c76fe
peter <taphuong.cmc@gmail.com>
2023-01-12 10:10:11 +0700

    rabsing

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     <div :class="cIsDarkMode ? 'dark' : 'light'">
         <div class="app">
             <router-view name="Header" />
+            <Pagination />
             <router-view class="body" />
             <router-view name="Footer" />            
         </div>
@@ -11,6 +12,9 @@
 <script setup lang="ts" name="App">
 import { useStore } from '@/store';
 import { computed } from 'vue';
+import { MutationTypes } from '@/store/mutations';
+import Pagination from '@/components/common/pagination/index.vue';
+
 const store = useStore();
 const cIsDarkMode = computed(() => store.state.gDarkMode);
 </script>
--- src/components/common/pagination/index.vue
+++ src/components/common/pagination/index.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>ComboboxTime</div>
+    <v-pagination :size="'x-small'" :active-color="'#2ec0df'" :color="'#9ca2ab'" :length="6" :dark="true"></v-pagination>
 </template>
 
-<script setup lang="ts" name="ButtonCreate"></script>
+<script setup lang="ts" name="Pagination"></script>
 
 <style lang="scss" scoped>
 @import 'index.scss';
