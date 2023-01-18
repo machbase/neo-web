5308d0d58855767f62ed46c9d23b4229652058c2
CMCGTVNgoc <tongocsam2409@gmail.com>
2023-01-18 09:45:13 +0700

    .

--- src/components/common/date-picker/index.vue
+++ src/components/common/date-picker/index.vue
@@ -1,8 +1,29 @@
 <template>
-    <div>DatePicker</div>
+    <Datepicker v-model="sDate" class="date-picker" :format="'yyyy-MM-dd HH:mm'" position="left" :dark="cIsDarkMode">
+        <template #input-icon>
+            <img class="input-slot-image" :src="icon" />
+        </template>
+    </Datepicker>
 </template>
 
-<script setup lang="ts" name="DatePicker"></script>
+<script setup lang="ts" name="DatePicker">
+import icon from '@/assets/image/ic_calendar.svg';
+import { useStore } from '@/store';
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+import { defineEmits, ref, watch, computed } from 'vue';
+
+const emit = defineEmits(['eChangeTime']);
+const store = useStore();
+const cIsDarkMode = computed(() => store.getters.getDarkMode);
+const sDate = ref<string>('');
+watch(
+    () => sDate.value,
+    () => {
+        emit('eChangeTime', sDate.value);
+    }
+);
+</script>
 
 <style lang="scss" scoped>
 @import 'index.scss';
