23695b617c70ea56e740d8d60adab0ab9f47986b
peter <taphuong.cmc@gmail.com>
2023-01-18 09:30:48 +0700

    rebasing

--- src/components/popup-list/popup/TimeRange.vue
+++ src/components/popup-list/popup/TimeRange.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="time-range-wrapper">
         <div class="col-left">
-            <div>From<Datepicker v-model="dateStart"></Datepicker></div>
-            <div>To<Datepicker v-model="dateEnd"></Datepicker></div>
+            <div>
+                From
+                <Datepicker v-model="dateStart" auto-apply :dark="false">
+                    <template #trigger>
+                        <span>Img</span>
+                        <input :value="dateStart"/>
+                        <!-- <span><img src="" /></span> -->
+                    </template>
+                </Datepicker>
+            </div>
+            <div>To<Datepicker v-model="dateEnd" text-input auto-apply></Datepicker></div>
             <div>
                 <p>Refreshing every</p>
                 <div class="row"><ComboboxTime /><v-btn variant="outlined"> Apply </v-btn></div>
@@ -18,8 +27,23 @@ import '@vuepic/vue-datepicker/dist/main.css';
 import ComboboxTime from '@/components/common/combobox/combobox-time/index.vue';
 import TimeRange from '@/components/common/date-list/date-time-range.vue';
 import { computed, defineEmits, reactive, ref } from 'vue';
+const timeRange = reactive<any>({
+    start: '',
+    end: '',
+});
 const OnTimeRange = (data: any) => {
     console.log('🚀 ~ file: index.vue:26 ~ onClickScale ~ data', data);
+    timeRange.start = data.value[0];
+    dateStart.value = data.value[0];
+    timeRange.end = data.value[1];
+    console.log('🚀 ~ file: TimeRange.vue:29 ~ OnTimeRange ~ timeRange', timeRange);
+};
+const value = timeRange.start;
+const format = (date: any) => {
+    // const day = date.getDate();
+    // const month = date.getMonth() + 1;
+    // const year = date.getFullYear();
+    return `Selected date is ${timeRange.start}`;
 };
 const emit = defineEmits(['eClosePopup']);
 const dateStart = ref();
