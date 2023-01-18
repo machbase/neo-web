34f5bc6dc5b624704a40a91d88ed53e39ffc0de0
CMCGTVNgoc <tongocsam2409@gmail.com>
2023-01-18 10:24:35 +0700

    .

--- src/components/common/date-picker/index.vue
+++ src/components/common/date-picker/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <Datepicker v-model="sDate" class="date-picker" :format="'yyyy-MM-dd HH:mm'" position="left" :dark="cIsDarkMode">
+    <Datepicker v-model="sDate" class="date-picker" :format="'yyyy-MM-dd HH:mm'" position="left" :dark="cIsDarkMode" :disabled="pDisabled">
         <template #input-icon>
             <img class="input-slot-image" :src="icon" />
         </template>
@@ -11,8 +11,11 @@ import icon from '@/assets/image/ic_calendar.svg';
 import { useStore } from '@/store';
 import Datepicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
-import { defineEmits, ref, watch, computed } from 'vue';
-
+import { defineEmits, defineProps, ref, watch, computed } from 'vue';
+export interface DatePickerProps {
+    pDisabled: boolean;
+}
+const props = defineProps<DatePickerProps>();
 const emit = defineEmits(['eChangeTime']);
 const store = useStore();
 const cIsDarkMode = computed(() => store.getters.getDarkMode);
