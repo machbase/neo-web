59a00b5b36c4eff64a9afbc76a28a9a8f07b6ef7
CMCGTVNgoc <tongocsam2409@gmail.com>
2023-01-19 09:24:18 +0700

    .

--- src/pages/tag-view/index.vue
+++ src/pages/tag-view/index.vue
@@ -1,23 +1,27 @@
 <template>
     <div class="tag-view">
-        <ChartDashboard />
+        <ChartDashboard v-for="(aPanel, aIndex) in sData?.panels" :key="aIndex" />
         <ButtonCreate :is-add-chart="true" :on-click="onOpenPopup" />
         <PopupWrap :width="'500px'" :p-type="PopupType.NEW_CHART" :p-show="sDialog" @e-close-popup="onClosePopup" />
     </div>
 </template>
 <script setup lang="ts" name="TagView">
-import CustomScale, { CustomScaleInput } from '@/components/common/custom-scale/index.vue';
-import Pagination from '@/components/common/pagination/index.vue';
+import { getBoard } from '@/api/repository/api';
 import ButtonCreate from '@/components/common/button-create/index.vue';
 import ChartDashboard from '@/components/common/chart-dashboard/index.vue';
 import PopupWrap from '@/components/popup-list/index.vue';
 import { PopupType } from '@/enums/app';
-import { ref } from 'vue';
-import { getBoard } from '@/api/repository/api';
+import { BoardInfo } from '@/interface/chart';
+import { ResBoardList } from '@/interface/tagView';
+import { useStore } from '@/store';
+import { computed, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
+const store = useStore();
 const sDialog = ref<boolean>(false);
+const sData = ref<BoardInfo>();
+const cBoardList = computed((): ResBoardList[] => store.state.gBoardList);
 
 function onOpenPopup() {
     sDialog.value = true;
@@ -27,9 +31,29 @@ const onClosePopup = () => {
 };
 
 const setBoard = async (sId: string) => {
-    const sRes = await getBoard(sId);
+    const sRes: BoardInfo = await getBoard(sId);
+    sData.value = sRes;
 };
-setBoard(route.query.id as string);
+
+watch(
+    () => route.query.id,
+    () => {
+        if (route.query.id) {
+            setBoard(route.query.id as string);
+        }
+    }
+);
+watch(
+    () => cBoardList.value,
+    () => {
+        if (!route.query.id && cBoardList.value.length > 0) {
+            setBoard(cBoardList.value[0]?.board_id as string);
+        }
+        if (route.query.id) {
+            setBoard(route.query.id as string);
+        }
+    }
+);
 </script>
 
 <style lang="scss" scoped>
