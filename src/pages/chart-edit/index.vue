c626673f47d604f2a299adadf8cc979ce8485db7
peter <taphuong.cmc@gmail.com>
2023-02-09 15:36:55 +0700

    doing

--- src/pages/chart-edit/index.vue
+++ src/pages/chart-edit/index.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="chart-edit-page">
         <ChartDashboard />
-        <TimeRangeTab/>
+        <DataTab/>
+        <!-- <TimeRangeTab/> -->
     </div>
 </template>
 <script setup lang="ts" name="ChartEdit">
 import ChartDashboard from '@/components/common/chart-dashboard/index.vue';
 import TimeRangeTab from '../chart-edit/components/time-range/index.vue';
+import DataTab from '../chart-edit/components/data/index.vue';
 import { ref } from 'vue';
 </script>
 
