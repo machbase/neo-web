907c53c78d270f8be6bb3b45ea4894066fe49bd9
kevin-lee <kevin.lee@machbase.com>
2023-08-24 10:36:16 +0900

    machbase/neo-web update chart axis

--- src/utils/sqlFormatter.ts
+++ src/utils/sqlFormatter.ts
@@ -8,6 +8,22 @@ export const sqlBasicFormatter = (aSql: string, aLimit: number, aFormat: string,
     return 'SQL(`' + aSql + '`)\n' + 'DROP(' + (aLimit * 50 - 50) + `)\nTAKE(50)\nJSON(timeformat('` + aFormat + `'), tz('` + aTimezone + `'))`;
 };
 
-export const sqlBasicChartFormatter = (aSql: string, aWidth: number, aHeight: number) => {
-    return 'SQL(`' + aSql + '`)\n' + 'TAKE(100)\n' + 'CHART_LINE(size($w ?? ' + `'${aWidth}px',$h ??'${aHeight}px'))`;
+export const sqlBasicChartFormatter = (aSql: string, aWidth: number, aHeight: number, aAxis?: { x: string; y: string }) => {
+    if (aAxis)
+        return (
+            'SQL(`' +
+            aSql +
+            '`)\n' +
+            'TAKE(5000)\n' +
+            'CHART_LINE(' +
+            `xAxis(0,'` +
+            aAxis.x +
+            `'), yAxis(1, '` +
+            aAxis.y +
+            `'),` +
+            `dataZoom('slider', 0, 100),` +
+            'size($w ?? ' +
+            `'${aWidth}px',$h ??'${aHeight}px'))`
+        );
+    else return 'SQL(`' + aSql + '`)\n' + 'TAKE(5000)\n' + 'CHART_LINE(size($w ?? ' + `'${aWidth}px',$h ??'${aHeight}px'))`;
 };
