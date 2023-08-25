ad3c72cf0666b674ff36a141742261a87adca5f8
kevin-lee <kevin.lee@machbase.com>
2023-08-25 14:47:28 +0900

    machbase/neo-web#359 update save modal ui

--- src/components/modal/SaveModal.tsx
+++ src/components/modal/SaveModal.tsx
@@ -463,11 +463,13 @@ export const SaveModal = (props: SaveModalProps) => {
                                             className={`row ${sSelectedFile && sSelectedFile.name === aItem.name ? 'selected' : ''}`}
                                             onClick={(aEvent) => handleSelectFile(aEvent, aItem)}
                                         >
-                                            <div className="pl" style={{ width: '50%', display: 'flex', alignItems: 'center', gap: '4px' }}>
-                                                {aItem.type === 'dir' ? <TreeFolder height={100} /> : <DotChart height={100} />}
+                                            <div className="pl" style={{ width: '48%', maxWidth: '48%', minWidth: '48%', display: 'flex', alignItems: 'center', gap: '4px' }}>
+                                                <div className="pl-icon" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                                    {aItem.type === 'dir' ? <TreeFolder height={100} /> : <DotChart height={100} />}
+                                                </div>
                                                 <span>{aItem.name}</span>
                                             </div>
-                                            <span className="pl" style={{ width: '30%' }}>
+                                            <span className="pl" style={{ width: '32%' }}>
                                                 {elapsedTime(aItem.lastModifiedUnixMillis)}
                                             </span>
                                             <span className="pl" style={{ width: '20%' }}>
