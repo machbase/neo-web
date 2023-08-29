2ff5c22ad42982808d2c7fcd804a88d29489e83c
jeff.park <jeff.park@machbase.com>
2023-08-29 14:40:21 +0900

    machbase/neo-web#380 refactoring icon button

--- src/components/editor/Body.tsx
+++ src/components/editor/Body.tsx
@@ -143,7 +143,7 @@ const Body = ({ pExtentionList, pSideSizes, pReferences, pDraged, pRecentFiles, 
                             {aItem.type === 'sql' && (
                                 <Sql pHandleSaveModalOpen={handleSaveModalOpen} pInfo={aItem} setIsOpenModal={setIsOpenModal} setIsSaveModal={setIsSaveModal}></Sql>
                             )}
-                            {aItem.type === 'tql' && <Tql pHandleSaveModalOpen={handleSaveModalOpen} setIsOpenModal={setIsOpenModal} setIsSaveModal={setIsSaveModal}></Tql>}
+                            {aItem.type === 'tql' && <Tql pHandleSaveModalOpen={handleSaveModalOpen} />}
                             {aItem.type === 'taz' && (
                                 <TagAnalyzer
                                     pHandleSaveModalOpen={handleSaveModalOpen}
