57c8da2a6fe7c13496138fed2e6fad7e7dc9478a
kevin-lee <kevin.lee@machbase.com>
2023-08-29 14:15:39 +0900

    machbase/neo-web#384 update sql parser

---
+++ src/utils/sqlQueryParser.ts
@@ -0,0 +1,129 @@
+/** POSITION INTERFACE */
+interface PositionType {
+    column: number;
+    lineNumber: number;
+}
+/** SELECTION INTERFACE */
+interface SelectionType {
+    endColumn: number;
+    endLineNumber: number;
+    positionColumn: number;
+    positionLineNumber: number;
+    selectionStartColumn: number;
+    selectionStartLineNumber: number;
+    startColumn: number;
+    startLineNumber: number;
+}
+/** REMOVE ANNOTATION */
+const removeAnnotation = (aSplitQuery: string[]): string[] => {
+    return aSplitQuery
+        .map((aQuery: string) => {
+            if (aQuery.includes('--') && !!aQuery.split('--')[0].trim()) {
+                return aQuery.split('--')[0];
+            }
+            if (aQuery.includes('--')) return;
+            return aQuery;
+        })
+        .filter((aItem: string | undefined) => aItem !== undefined) as string[];
+};
+/** CHECK SELECTION ANNOTATION LINE */
+const isSelectAnnotationLine = (aSplitQuery: string[], aPosition: PositionType): boolean => {
+    return aSplitQuery[aPosition.lineNumber - 1].includes('--') && !aSplitQuery[aPosition.lineNumber - 1].split('--')[0].trim();
+};
+/** CHECK BLANK QUERY */
+const isBlankQuery = (aQuery: string): boolean => {
+    return aQuery.trim() === '';
+};
+/** FIND VARIABLE & PARSING QUERY */
+const findVariableNParsedQuery = (aNoAnnotationList: string[]) => {
+    const sRegExp = new RegExp("([']*'[^']*')", 'igm');
+    let sParsedQuery: string = aNoAnnotationList.join('\n');
+    let sVariableList: { index: number | undefined; value: string; replaceValue: string }[] = [];
+
+    if (sParsedQuery.match(sRegExp)) {
+        sVariableList = sParsedQuery.match(sRegExp).map((aString: string) => {
+            return { value: aString, index: (sRegExp.exec(sParsedQuery) as any).index, replaceValue: aString.replaceAll(';', 'M') };
+        });
+        sVariableList.map((aVariable) => {
+            sParsedQuery = sParsedQuery.replace(aVariable.value, aVariable.replaceValue);
+        });
+    }
+    return { sParsedQuery, sVariableList };
+};
+/** FIND START & END LINE */
+const findStartNEndLine = (aSplitQueryList: string[], aSelection: SelectionType): { sStartLine: number; sEndLine: number } => {
+    let sStartLine: number = aSelection.startLineNumber - 1;
+    let sEndLine: number = aSelection.endLineNumber - 1;
+    aSplitQueryList.map((aQuery: string, aIdx: number) => {
+        if (aQuery.includes('--') && aIdx <= sStartLine) {
+            if (aQuery.split('--')[0].trim() === '') sStartLine -= 1;
+        }
+        if (aQuery.includes('--') && aIdx <= sEndLine) {
+            if (aQuery.split('--')[0].trim() === '') sEndLine -= 1;
+        }
+    });
+    return { sStartLine, sEndLine };
+};
+/** FIND CURSOR LENGTH */
+const findCursorLength = (aNoAnnotationList: string[], aStartLine: number, aEndLine: number, aSelection: SelectionType) => {
+    let rTotalLen = 0;
+    let sSelectionLen = 0;
+
+    aNoAnnotationList.map((aRow: string, aIdx: number) => {
+        if (aStartLine <= aIdx && aIdx <= aEndLine) {
+            sSelectionLen = rTotalLen + aSelection.endColumn - 1 + aIdx;
+            if (sSelectionLen === 0) sSelectionLen = 1;
+        }
+        rTotalLen += aRow.length;
+    });
+
+    return sSelectionLen;
+};
+/** FIND TARGET QUERY */
+const findTargetQuery = (
+    aParsedQuery: string,
+    aSelectionLen: number,
+    aVariableList: {
+        index: number | undefined;
+        value: string;
+        replaceValue: string;
+    }[]
+) => {
+    let semiTotalLen = 0;
+    let targetQuery = '';
+    const sSemiList = aParsedQuery.split(';');
+    sSemiList.map((aRow: string, aIdx: number) => {
+        if (semiTotalLen < aSelectionLen) {
+            targetQuery = sSemiList[aIdx];
+            if (sSemiList[aIdx].trim() === '') targetQuery = sSemiList[aIdx - 1];
+            if (targetQuery && targetQuery.includes("'")) {
+                aVariableList.map((aVar, aIdx: number) => {
+                    if (targetQuery.includes(aVar.replaceValue)) {
+                        targetQuery = targetQuery.replace(aVariableList[aIdx].replaceValue, aVariableList[aIdx].value);
+                    }
+                });
+            }
+        }
+        semiTotalLen += aRow.length + 1;
+    });
+
+    return targetQuery.split('\n').join(' ').trim();
+};
+/**
+ * SQL QUERY PARSER
+ * @QUERY_TEXT string;
+ * @POSITION PositionType
+ * @SELECTION SelectionType
+ */
+export const sqlQueryParser = (aQueryTxt: string, aPosition: PositionType, aSelection: SelectionType) => {
+    if (isBlankQuery(aQueryTxt)) return '';
+    if (!aPosition) return '';
+    if (!aSelection) return '';
+    const sSplitQueryList = JSON.parse(JSON.stringify(aQueryTxt)).split('\n');
+    if (isSelectAnnotationLine(sSplitQueryList, aPosition)) return '';
+    const sNoAnnotationList = removeAnnotation(sSplitQueryList);
+    const { sStartLine, sEndLine } = findStartNEndLine(sSplitQueryList, aSelection);
+    const { sParsedQuery, sVariableList } = findVariableNParsedQuery(sNoAnnotationList);
+    const sSelectionLen = findCursorLength(sNoAnnotationList, sStartLine, sEndLine, aSelection);
+    return findTargetQuery(sParsedQuery, sSelectionLen, sVariableList);
+};
