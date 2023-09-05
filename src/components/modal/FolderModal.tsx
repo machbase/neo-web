7308a21bea9a59fc1706a2cef2c2b9caedc15d5d
kevin-lee <kevin.lee@machbase.com>
2023-09-05 17:18:11 +0900

    machbase/neo-web#296 update folder modal

--- src/components/modal/FolderModal.tsx
+++ src/components/modal/FolderModal.tsx
@@ -1,25 +1,26 @@
-import { useRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { gRecentDirectory } from '@/recoil/fileTree';
 import { BsGit, Close } from '@/assets/icons/Icon';
 import { TextButton } from '../buttons/TextButton';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Modal from './Modal';
 
 import './FolderModal.scss';
 import { postFileList } from '@/api/repository/api';
+import useDebounce from '@/hooks/useDebounce';
 
 export interface FolderModalProps {
     setIsOpen: any;
     pIsDarkMode?: boolean;
-    pReName?: { isReName: boolean; curName: string };
 }
 
 export const FolderModal = (props: FolderModalProps) => {
-    const { setIsOpen, pIsDarkMode, pReName } = props;
+    const { setIsOpen, pIsDarkMode } = props;
     const [sFolderName, setFolderName] = useState<string>('');
     const [sGitUrl, setGitUrl] = useState<string>('https://github.com/machbase/neo-samples.git');
-    const [sRecentDirectory, setRecentDirectory] = useRecoilState(gRecentDirectory);
+    const sRecentDirectory = useRecoilValue(gRecentDirectory);
     const [sFolderPath, setFolderPath] = useState<string>('');
+    const sGitUrlRef: any = useRef(null);
 
     const handleClose = () => {
         setIsOpen(false);
@@ -30,21 +31,11 @@ export const FolderModal = (props: FolderModalProps) => {
         let sPath = '';
         const sDirectoryList = (sRecentDirectory as string).split('/').filter((aDir: string) => aDir);
         let sPayload: any = {};
-
         if (sDirectoryList.length > 0) {
             sPath = sDirectoryList.join('/') + '/' + sFolderName;
         } else sPath = sFolderName;
-
-        // GIT
         if (sGitUrl) {
             sPayload = { url: sGitUrl };
-            // const sRegUrl = new RegExp(/^http[s]?:\/\/github.com/i);
-            // if (sGitUrl.includes('.git') && sRegUrl.test(sGitUrl)) {
-            //     console.log('.git 도 포함');
-            // }
-            // if (!sGitUrl.includes('.git') && sRegUrl.test(sGitUrl)) {
-            //     console.log('.git 은 미포함');
-            // }
         } else {
             sPayload = {
                 isDir: true,
@@ -52,28 +43,27 @@ export const FolderModal = (props: FolderModalProps) => {
             };
         }
 
-        console.log('reuslt', sPath);
-        console.log('payload', sPayload);
-        const sResult = await postFileList(sPayload, sPath, '');
-        console.log('post result', sResult);
-
-        // postFileList
-        // * GitCloneReq
-        // { url: ''}
-
-        // GIT URL EX
-        // 1. https://github.com/machbase/neo-samples.git
-        // 2. https://github.com/machbase/neo-samples
-
-        // handleClose()
+        const sResult: any = await postFileList(sPayload, sPath, '');
+        if (sResult && sResult.success) {
+            handleClose();
+        } else {
+            console.error('');
+        }
     };
+
+    const handleFoldername = () => {
+        if (!sGitUrl) return;
+        if (sFolderName) return;
+        const sPathList = sGitUrl.split('/');
+        if (sPathList.length <= 0) return;
+        setFolderName(sPathList[sPathList.length - 1].split('.')[0]);
+    };
+
+    useDebounce(sGitUrlRef, [sGitUrl], handleFoldername);
 
     useEffect(() => {
         if (setIsOpen) {
             setFolderPath(sRecentDirectory as string);
-            if (pReName) {
-                console.log('rename');
-            }
         }
     }, [setIsOpen]);
 
@@ -106,7 +96,7 @@ export const FolderModal = (props: FolderModalProps) => {
                                     <span>Clone</span>
                                 </div>
                                 <div className={`input-wrapper ${pIsDarkMode ? 'input-wrapper-dark' : ''}`}>
-                                    <input onChange={(e: any) => setGitUrl(e.target.value)} value={sGitUrl} />
+                                    <input ref={sGitUrlRef} onChange={(e: any) => setGitUrl(e.target.value)} value={sGitUrl} />
                                 </div>
                             </div>
                         </div>
