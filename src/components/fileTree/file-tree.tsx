0e3fd483fd8783472df52fd33e82fbf4e9dd875c
kevin-lee <kevin.lee@machbase.com>
2023-10-24 14:56:51 +0900

    machbase/neo-web#483 update file | dir path

--- src/components/fileTree/file-tree.tsx
+++ src/components/fileTree/file-tree.tsx
@@ -11,6 +11,7 @@ import { postFileList } from '@/api/repository/api';
 import { findItemByUniqueKey, findParentDirByUniqueKey } from '@/utils/file-manager';
 import useThrottle from '@/hooks/useThrottle';
 import { moveFile } from '@/api/repository/fileTree';
+import { FileNameValidator } from '@/utils/FileExtansion';
 
 interface FileTreeProps {
     rootDir: FileTreeType;
@@ -651,7 +652,7 @@ const FileDiv = ({
         if (e.code === 'Enter') {
             e.stopPropagation();
             if (sName && sName.length > 0) {
-                onRename(file, sName);
+                if (FileNameValidator(sName)) onRename(file, sName);
                 resetRenameValue();
             }
         }
@@ -663,7 +664,7 @@ const FileDiv = ({
     };
     const handleBlur = () => {
         if (sName && sName.length > 0) {
-            onRename(file, sName);
+            if (FileNameValidator(sName)) onRename(file, sName);
             resetRenameValue();
         } else resetRenameValue();
     };
@@ -728,7 +729,7 @@ const FileDiv = ({
                             <input
                                 type="text"
                                 value={sName}
-                                onChange={(e: any) => setName(e.target.value)}
+                                onChange={(e) => FileNameValidator(e.target.value) ? setName(e.target.value) : null}
                                 autoFocus
                                 onFocus={(e) => e.target.setSelectionRange(0, sName.length)}
                                 onBlur={handleBlur}
