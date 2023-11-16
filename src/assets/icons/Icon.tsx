f29287453f2ddac5ce14a94656720ac0b99b287b
jeff.park <jeff.park@machbase.com>
2023-11-16 18:14:04 +0900

    machbase/neo-web refactor react icons svg

--- src/assets/icons/Icon.tsx
+++ src/assets/icons/Icon.tsx
@@ -1,103 +1,199 @@
-export {
-    AiFillPlusCircle as PlusCircle,
-    AiOutlineDotChart as DotChart,
-    AiFillHome as Home,
-    AiOutlineBarChart as BarChart,
-    AiOutlineFileDone,
-    AiOutlineSnippets,
-    AiOutlineFileMarkdown,
-    AiFillGithub,
-} from 'react-icons/ai';
+// export {
+//     AiFillPlusCircle as PlusCircle,
+//     AiOutlineDotChart as DotChart,
+//     AiFillHome as Home,
+//     AiOutlineBarChart as BarChart,
+//     AiOutlineFileDone,
+//     AiOutlineSnippets,
+//     AiOutlineFileMarkdown,
+//     AiFillGithub,
+// } from 'react-icons/ai';
+export { ReactComponent as BarChart } from '@/assets/icons/react-icons/ai/bar-chart.svg'; // AiOutlineBarChart
+export { ReactComponent as DotChart } from '@/assets/icons/react-icons/ai/dot-chart.svg'; // AiOutlineDotChart
+export { ReactComponent as PlusCircle } from '@/assets/icons/react-icons/ai/plus-circle.svg'; // AiFillPlusCircle
+export { ReactComponent as Home } from '@/assets/icons/react-icons/ai/home.svg'; // AiFillHome
+export { ReactComponent as AiOutlineFileDone } from '@/assets/icons/react-icons/ai/file-done.svg';
+export { ReactComponent as AiOutlineFileMarkdown } from '@/assets/icons/react-icons/ai/file-markdown.svg';
+export { ReactComponent as AiFillGithub } from '@/assets/icons/react-icons/ai/github.svg';
+export { ReactComponent as AiOutlineSnippets } from '@/assets/icons/react-icons/ai/snippets.svg';
 
-export {
-    VscChevronRight,
-    VscChevronLeft,
-    VscDiffAdded,
-    VscWarning,
-    VscSync,
-    VscRecord,
-    VscLibrary,
-    VscTrash,
-    VscTerminalCmd as Cmd,
-    VscSymbolFile,
-    VscThreeBars,
-    VscTerminal,
-    VscNote,
-    VscPass,
-    VscGraphLine,
-    VscGear as Gear,
-    VscFiles,
-    VscNotebook,
-    VscFile,
-    VscGraphScatter,
-    VscVm,
-    VscChevronDown,
-    VscChevronUp,
-    VscTerminalCmd,
-    VscTerminalPowershell as Powershell,
-    VscAdd,
-    VscNewFile,
-    VscJson,
-} from 'react-icons/vsc';
+// export {
+//     VscChevronRight,
+//     VscChevronLeft,
+//     VscDiffAdded,
+//     VscWarning,
+//     VscSync,
+//     VscRecord,
+//     VscLibrary,
+//     VscTrash,
+//     VscTerminalCmd as Cmd,
+//     VscSymbolFile,
+//     VscThreeBars,
+//     VscTerminal,
+//     VscNote,
+//     VscPass,
+//     VscGraphLine,
+//     VscGear as Gear,
+//     VscFiles,
+//     VscNotebook,
+//     VscFile,
+//     VscGraphScatter,
+//     VscVm,
+//     VscChevronDown,
+//     VscChevronUp,
+//     VscTerminalPowershell as Powershell,
+//     VscAdd,
+//     VscNewFile,
+//     VscJson,
+// } from 'react-icons/vsc';
+export { ReactComponent as VscChevronRight } from '@/assets/icons/react-icons/vsc/chevron-right.svg';
+export { ReactComponent as VscChevronLeft } from '@/assets/icons/react-icons/vsc/chevron-left.svg';
+export { ReactComponent as VscDiffAdded } from '@/assets/icons/react-icons/vsc/diff-added.svg';
+export { ReactComponent as VscWarning } from '@/assets/icons/react-icons/vsc/warning.svg';
+export { ReactComponent as VscSync } from '@/assets/icons/react-icons/vsc/sync.svg';
+export { ReactComponent as VscRecord } from '@/assets/icons/react-icons/vsc/record.svg';
+export { ReactComponent as VscLibrary } from '@/assets/icons/react-icons/vsc/library.svg';
+export { ReactComponent as VscTrash } from '@/assets/icons/react-icons/vsc/trash.svg';
+export { ReactComponent as Cmd } from '@/assets/icons/react-icons/vsc/terminal-cmd.svg'; // VscTerminalCmd
+export { ReactComponent as VscSymbolFile } from '@/assets/icons/react-icons/vsc/symbol-file.svg';
+export { ReactComponent as VscThreeBars } from '@/assets/icons/react-icons/vsc/three-bars.svg';
+export { ReactComponent as VscTerminal } from '@/assets/icons/react-icons/vsc/terminal.svg';
+export { ReactComponent as VscNote } from '@/assets/icons/react-icons/vsc/note.svg';
+export { ReactComponent as VscPass } from '@/assets/icons/react-icons/vsc/pass.svg';
+export { ReactComponent as VscGraphLine } from '@/assets/icons/react-icons/vsc/graph-line.svg';
+export { ReactComponent as Gear } from '@/assets/icons/react-icons/vsc/gear.svg'; // VscGear
+export { ReactComponent as VscFiles } from '@/assets/icons/react-icons/vsc/files.svg';
+export { ReactComponent as VscNotebook } from '@/assets/icons/react-icons/vsc/notebook.svg';
+export { ReactComponent as VscFile } from '@/assets/icons/react-icons/vsc/file.svg';
+export { ReactComponent as VscGraphScatter } from '@/assets/icons/react-icons/vsc/graph-scatter.svg';
+export { ReactComponent as VscVm } from '@/assets/icons/react-icons/vsc/vm.svg';
+export { ReactComponent as VscChevronDown } from '@/assets/icons/react-icons/vsc/chevron-down.svg';
+export { ReactComponent as VscChevronUp } from '@/assets/icons/react-icons/vsc/chevron-up.svg';
+export { ReactComponent as Powershell } from '@/assets/icons/react-icons/vsc/terminal-powershell.svg'; // VscTerminalPowershell
+export { ReactComponent as VscAdd } from '@/assets/icons/react-icons/vsc/add.svg';
+export { ReactComponent as VscNewFile } from '@/assets/icons/react-icons/vsc/new-file.svg';
+export { ReactComponent as VscJson } from '@/assets/icons/react-icons/vsc/json.svg';
 
-export {
-    MdOutlineStackedLineChart,
-    MdVpnKey as Key,
-    MdClose as Close,
-    MdSave as Save,
-    MdSaveAs as SaveAs,
-    MdCreateNewFolder as NewFolder,
-    MdDelete as Delete,
-    MdDownload as Download,
-    MdCenterFocusStrong,
-    MdMonitorHeart,
-    MdOutlineDashboard as MdDashboard,
-    MdOutlineKeyboardArrowDown as ArrowDown,
-    MdOutlineKeyboardArrowLeft as ArrowLeft,
-    MdOutlineKeyboardArrowRight as ArrowRight,
-    MdRawOn,
-    MdRefresh,
-    MdOutlineUpgrade,
-    MdFlagCircle,
-    MdOutlineCancel as Cancel,
-    MdLink,
-} from 'react-icons/md';
+// export {
+//     MdOutlineStackedLineChart,
+//     MdVpnKey as Key,
+//     MdClose as Close,
+//     MdSave as Save,
+//     MdSaveAs as SaveAs,
+//     MdCreateNewFolder as NewFolder,
+//     MdDelete as Delete,
+//     MdDownload as Download,
+//     MdCenterFocusStrong,
+//     MdMonitorHeart,
+//     MdOutlineDashboard as MdDashboard,
+//     MdOutlineKeyboardArrowDown as ArrowDown,
+//     MdOutlineKeyboardArrowLeft as ArrowLeft,
+//     MdOutlineKeyboardArrowRight as ArrowRight,
+//     MdRawOn,
+//     MdRefresh,
+//     MdOutlineUpgrade,
+//     MdFlagCircle,
+//     MdOutlineCancel as Cancel,
+//     MdLink,
+// } from 'react-icons/md';
+export { ReactComponent as MdOutlineStackedLineChart } from '@/assets/icons/react-icons/md/stacked-line-chart.svg';
+export { ReactComponent as Key } from '@/assets/icons/react-icons/md/key.svg'; // MdVpnKey
+export { ReactComponent as Close } from '@/assets/icons/react-icons/md/close.svg'; // MdClose
+export { ReactComponent as Save } from '@/assets/icons/react-icons/md/content-save.svg'; // MdSave
+export { ReactComponent as SaveAs } from '@/assets/icons/react-icons/md/content-save-edit.svg'; // MdSaveAs
+export { ReactComponent as NewFolder } from '@/assets/icons/react-icons/md/create-new-folder.svg'; // MdCreateNewFolder
+export { ReactComponent as Delete } from '@/assets/icons/react-icons/md/delete.svg'; // MdDelete
+export { ReactComponent as Download } from '@/assets/icons/react-icons/md/download.svg'; // MdDownload
+export { ReactComponent as MdCenterFocusStrong } from '@/assets/icons/react-icons/md/image-filter-center-focus-strong.svg';
+// export { ReactComponent as MdMonitorHeart } from '@/assets/icons/react-icons/md/.svg';
+export { ReactComponent as MdDashboard } from '@/assets/icons/react-icons/md/view-dashboard.svg'; // MdOutlineDashboard
+export { ReactComponent as ArrowDown } from '@/assets/icons/react-icons/md/chevron-down.svg'; // MdOutlineKeyboardArrowDown
+export { ReactComponent as ArrowLeft } from '@/assets/icons/react-icons/md/chevron-left.svg'; // MdOutlineKeyboardArrowLeft
+export { ReactComponent as ArrowRight } from '@/assets/icons/react-icons/md/chevron-right.svg'; // MdOutlineKeyboardArrowRight
+export { ReactComponent as MdRawOn } from '@/assets/icons/react-icons/md/raw.svg';
+export { ReactComponent as MdRefresh } from '@/assets/icons/react-icons/md/refresh.svg';
+export { ReactComponent as MdOutlineUpgrade } from '@/assets/icons/react-icons/md/outline-upgrade.svg';
+export { ReactComponent as MdFlagCircle } from '@/assets/icons/react-icons/md/flag-circle.svg';
+export { ReactComponent as Cancel } from '@/assets/icons/react-icons/md/outline-cancel.svg'; // MdOutlineCancel
+export { ReactComponent as MdLink } from '@/assets/icons/react-icons/md/link.svg';
 
-export { RxUpdate as Update } from 'react-icons/rx';
+// export { RxUpdate as Update } from 'react-icons/rx';
+export { ReactComponent as Update } from '@/assets/icons/react-icons/rx/update.svg'; // RxUpdate
 
-export {
-    HiTableCells as TableHeader,
-    HiOutlineTableCells as TableNotHeader,
-    HiOutlineClipboardDocumentList as Worksheet,
-    HiOutlineDocumentText as Document,
-} from 'react-icons/hi2';
+// export {
+//     HiTableCells as TableHeader,
+//     HiOutlineTableCells as TableNotHeader,
+//     HiOutlineClipboardDocumentList as Worksheet,
+//     HiOutlineDocumentText as Document,
+// } from 'react-icons/hi2';
+export { ReactComponent as TableHeader } from '@/assets/icons/react-icons/hi2/table-cells.svg'; // HiTableCells
+export { ReactComponent as TableNotHeader } from '@/assets/icons/react-icons/hi2/outline-table-cells.svg'; // HiOutlineTableCells
+export { ReactComponent as Worksheet } from '@/assets/icons/react-icons/hi2/clipboard-document-list.svg'; // HiOutlineClipboardDocumentList
+export { ReactComponent as Document } from '@/assets/icons/react-icons/hi2/document-text.svg'; // HiOutlineDocumentText
 
-export { RiArrowUpDoubleFill as ArrowUpDouble, RiMergeCellsVertical as HideOn, RiSplitCellsHorizontal as HideOff, RiSmartphoneLine as Phone } from 'react-icons/ri';
+// export { RiArrowUpDoubleFill as ArrowUpDouble, RiMergeCellsVertical as HideOn, RiSplitCellsHorizontal as HideOff, RiSmartphoneLine as Phone } from 'react-icons/ri';
+export { ReactComponent as ArrowUpDouble } from '@/assets/icons/react-icons/ri/arrow-up-double-fill.svg'; // RiArrowUpDoubleFill
+export { ReactComponent as HideOn } from '@/assets/icons/react-icons/ri/merge-cells-vertical.svg'; // RiMergeCellsVertical
+export { ReactComponent as HideOff } from '@/assets/icons/react-icons/ri/merge-cells-horizontal.svg'; // RiSplitCellsHorizontal
+export { ReactComponent as Phone } from '@/assets/icons/react-icons/ri/smartphone-line.svg'; // RiSmartphoneLine
 
-export { FcFolder as TreeFolder, FcOpenedFolder as TreeFolderOpen } from 'react-icons/fc';
+// export { FcFolder as TreeFolder, FcOpenedFolder as TreeFolderOpen } from 'react-icons/fc';
+export { ReactComponent as TreeFolder } from '@/assets/icons/react-icons/fc/folder.svg'; // FcFolder
+export { ReactComponent as TreeFolderOpen } from '@/assets/icons/react-icons/fc/opened_folder.svg'; // FcOpenedFolder
 
-export { IoArrowBackOutline, IoLogOutOutline as Logout, IoPlayForwardSharp } from 'react-icons/io5';
+// export { IoArrowBackOutline, IoLogOutOutline as Logout, IoPlayForwardSharp } from 'react-icons/io5';
+export { ReactComponent as IoArrowBackOutline } from '@/assets/icons/react-icons/io5/arrow-back-outline.svg';
+export { ReactComponent as Logout } from '@/assets/icons/react-icons/io5/log-out-outline.svg'; // IoLogOutOutline
+export { ReactComponent as IoPlayForwardSharp } from '@/assets/icons/react-icons/io5/play-forward-sharp.svg';
 
-export { PiFileCsvThin, PiSelectionPlusBold } from 'react-icons/pi';
+// export { PiFileCsvThin, PiSelectionPlusBold } from 'react-icons/pi';
+export { ReactComponent as PiFileCsvThin } from '@/assets/icons/react-icons/pi/file-csv-thin.svg';
+export { ReactComponent as PiSelectionPlusBold } from '@/assets/icons/react-icons/pi/selection-plus-bold.svg';
 
-export { BiLineChart as LineChart, BiDownload, BiLink, BiPlay as Play, BiSolidDownArrow, BiSolidChart, BiSolidEdit } from 'react-icons/bi';
+// export { BiLineChart as LineChart, BiDownload, BiLink, BiPlay as Play, BiSolidDownArrow, BiSolidChart, BiSolidEdit } from 'react-icons/bi';
+export { ReactComponent as LineChart } from '@/assets/icons/react-icons/bi/bx-line-chart.svg'; // BiLineChart
+export { ReactComponent as BiDownload } from '@/assets/icons/react-icons/bi/bx-download.svg';
+export { ReactComponent as BiLink } from '@/assets/icons/react-icons/bi/bx-link.svg';
+export { ReactComponent as Play } from '@/assets/icons/react-icons/bi/bx-play.svg'; // BiPlay
+export { ReactComponent as BiSolidDownArrow } from '@/assets/icons/react-icons/bi/bxs-down-arrow.svg';
+export { ReactComponent as BiSolidChart } from '@/assets/icons/react-icons/bi/solid-chart.svg';
+export { ReactComponent as BiSolidEdit } from '@/assets/icons/react-icons/bi/bxs-edit.svg';
 
-export { FiSearch as Search, FiMonitor as Monitor } from 'react-icons/fi';
+// export { FiSearch as Search, FiMonitor as Monitor } from 'react-icons/fi';
+export { ReactComponent as Search } from '@/assets/icons/react-icons/fi/search.svg'; // FiSearch
+export { ReactComponent as Monitor } from '@/assets/icons/react-icons/fi/monitor.svg'; // FiMonitor
 
-export { TbSquarePlus, TbParachute, TbRowInsertTop as InsertRowTop, TbFolderPlus, TbFolderDown, TbFolder, TbCloudDown, TbEyeMinus } from 'react-icons/tb';
+// export { GoPencil, GoTerminal, GoDotFill, GoCommandPalette, GoDatabase } from 'react-icons/go';
+export { ReactComponent as GoPencil } from '@/assets/icons/react-icons/go/pencil.svg';
+export { ReactComponent as GoTerminal } from '@/assets/icons/react-icons/go/terminal.svg';
+export { ReactComponent as GoDotFill } from '@/assets/icons/react-icons/go/dot-fill.svg';
+export { ReactComponent as GoCommandPalette } from '@/assets/icons/react-icons/go/command-palette.svg';
+export { ReactComponent as GoDatabase } from '@/assets/icons/react-icons/go/database.svg';
 
+// export { LuFlipVertical, LuMonitorSpeaker } from 'react-icons/lu';
+export { ReactComponent as LuFlipVertical } from '@/assets/icons/react-icons/lu/flip-vertical.svg';
+export { ReactComponent as LuMonitorSpeaker } from '@/assets/icons/react-icons/lu/monitor-speaker.svg';
+
+// export { FaCircle as SaveCricle, FaLaptop, FaDesktop, FaRegCalendarAlt as Calendar, FaFolderPlus, FaUser } from 'react-icons/fa';
+export { ReactComponent as SaveCricle } from '@/assets/icons/react-icons/fa/circle-solid.svg'; // FaCircle
+export { ReactComponent as FaLaptop } from '@/assets/icons/react-icons/fa/laptop-solid.svg';
+export { ReactComponent as FaDesktop } from '@/assets/icons/react-icons/fa/desktop-solid.svg';
+export { ReactComponent as Calendar } from '@/assets/icons/react-icons/fa/calendar-days-solid.svg';
+export { ReactComponent as FaFolderPlus } from '@/assets/icons/react-icons/fa/folder-plus-solid.svg';
+export { ReactComponent as FaUser } from '@/assets/icons/react-icons/fa/user-solid.svg';
+
+// export { BsGit, BsTable } from 'react-icons/bs';
+export { ReactComponent as BsGit } from '@/assets/icons/react-icons/bs/git.svg';
+export { ReactComponent as BsTable } from '@/assets/icons/react-icons/bs/table.svg';
+
+// export { CgRename as Rename } from 'react-icons/cg';
+export { ReactComponent as Rename } from '@/assets/icons/react-icons/cg/rename.svg'; // CgRename
+
+// export { TfiLayoutColumn3Alt } from 'react-icons/tfi';
+export { ReactComponent as TfiLayoutColumn3Alt } from '@/assets/icons/react-icons/tfi/layout-column3-alt.svg';
+
+// 없거나 찾지 못한거
+export { MdMonitorHeart } from 'react-icons/md';
 export { IoIosCopy as Copy, IoIosArrowForward } from 'react-icons/io';
-
-export { GoPencil, GoTerminal, GoDotFill, GoCommandPalette, GoDatabase } from 'react-icons/go';
-
-export { LuFlipVertical, LuMonitorSpeaker } from 'react-icons/lu';
-
-export { FaCircle as SaveCricle, FaLaptop, FaDesktop, FaRegCalendarAlt as Calendar, FaFolderPlus, FaUser } from 'react-icons/fa';
-
 export { FaFolderOpen as FolderOpen, FaFolder as Folder, FaArrowRotateRight as Refresh, FaGear as GearFill, FaDatabase } from 'react-icons/fa6';
-
-export { BsGit, BsTable } from 'react-icons/bs';
-
-export { CgRename as Rename } from 'react-icons/cg';
-
-export { TfiLayoutColumn3Alt } from 'react-icons/tfi';
+export { TbSquarePlus, TbParachute, TbRowInsertTop as InsertRowTop, TbFolderPlus, TbFolderDown, TbFolder, TbCloudDown, TbEyeMinus } from 'react-icons/tb';
