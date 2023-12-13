a6f82a2488e6a093ddc30cdfe769dac6d2a5187b
kevin-lee <kevin.lee@machbase.com>
2023-12-13 14:39:06 +0900

    machbase/neo-web - init new chart

--- src/assets/ts/eChart.ts
+++ src/assets/ts/eChart.ts
@@ -1,23 +1,23 @@
-const sScriptFileUrl = [
-    `/web/echarts/echarts.min.js`,
-    `/web/echarts/echarts@4.min.js`,
-    `/web/echarts/echarts-gl.min.js`,
-    `/web/echarts/echarts-liquidfill.min.js`,
-    `/web/echarts/echarts-wordcloud.min.js`,
-    '/web/echarts/themes/essos.js',
-    '/web/echarts/themes/chalk.js',
-    '/web/echarts/themes/purple-passion.js',
-    '/web/echarts/themes/romantic.js',
-    '/web/echarts/themes/walden.js',
-    '/web/echarts/themes/westeros.js',
-    '/web/echarts/themes/wonderland.js',
-    '/web/echarts/themes/vintage.js',
-    '/web/echarts/themes/dark.js',
-    '/web/echarts/themes/macarons.js',
-    '/web/echarts/themes/infographic.js',
-    '/web/echarts/themes/shine.js',
-    '/web/echarts/themes/roma.js',
-];
+// const sScriptFileUrl = [
+//     `/web/echarts/echarts.min.js`,
+//     `/web/echarts/echarts@4.min.js`,
+//     `/web/echarts/echarts-gl.min.js`,
+//     `/web/echarts/echarts-liquidfill.min.js`,
+//     `/web/echarts/echarts-wordcloud.min.js`,
+//     '/web/echarts/themes/essos.js',
+//     '/web/echarts/themes/chalk.js',
+//     '/web/echarts/themes/purple-passion.js',
+//     '/web/echarts/themes/romantic.js',
+//     '/web/echarts/themes/walden.js',
+//     '/web/echarts/themes/westeros.js',
+//     '/web/echarts/themes/wonderland.js',
+//     '/web/echarts/themes/vintage.js',
+//     '/web/echarts/themes/dark.js',
+//     '/web/echarts/themes/macarons.js',
+//     '/web/echarts/themes/infographic.js',
+//     '/web/echarts/themes/shine.js',
+//     '/web/echarts/themes/roma.js',
+// ];
 
 const loadScript = (url: string) => {
     return new Promise((resolve, reject) => {
@@ -30,10 +30,10 @@ const loadScript = (url: string) => {
     });
 };
 
-const loadScriptsSequentially = async (scriptUrls: string[]) => {
+export const loadScriptsSequentially = async (scriptUrls: string[]) => {
     for (const url of scriptUrls) {
         await loadScript(url);
     }
 };
 
-loadScriptsSequentially(sScriptFileUrl);
+// loadScriptsSequentially(sScriptFileUrl);
--- src/plugin/eCharts.ts
+++ src/plugin/eCharts.ts
@@ -1,3 +1,7 @@
+import { loadScriptsSequentially } from '@/assets/ts/eChart';
+
+const JS_ASSETS_LIST: string[] = [];
+
 const showChart = (aData: any, sTheme: string) => {
     // @ts-ignore
     const goecharts_aibQDdRJHYEs = echarts.init(document.getElementById(aData.chartID), sTheme);
@@ -6,11 +10,25 @@ const showChart = (aData: any, sTheme: string) => {
     return;
 };
 
-export const drawChart = (aData: any, sTheme: string) => {
+const CheckAssets = (assets: string[]) => {
+    const newAssets: string[] = [];
+    assets.map((aAssets: string) => {
+        if (!JS_ASSETS_LIST.includes(aAssets)) {
+            JS_ASSETS_LIST.push(aAssets);
+            newAssets.push(aAssets);
+        }
+    });
+    return newAssets;
+};
+
+export const drawChart = async (aData: any, sTheme: string) => {
+    // aData.jsAssets
+    aData.jsAssets && (await loadScriptsSequentially(CheckAssets(aData.jsAssets)));
     // @ts-ignore
     const goecharts_aibQDdRJHYEs = echarts.init(document.getElementById(aData.chartID), sTheme);
 
     goecharts_aibQDdRJHYEs.setOption(aData.chartOption);
+    aData.chartAction && goecharts_aibQDdRJHYEs.dispatchAction(aData.chartAction);
     return goecharts_aibQDdRJHYEs;
 };
 
