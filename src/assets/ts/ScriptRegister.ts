e73bfe55b451b1a376dc097fea379b331ad29698
kevin-lee <kevin.lee@machbase.com>
2023-12-19 15:33:03 +0900

    machbase/neo-web#593 - update sql cahrt & script register

--- src/assets/ts/ScriptRegister.ts
+++ src/assets/ts/ScriptRegister.ts
@@ -1,20 +1,9 @@
-const JS_ASSETS_LIST: string[] = [];
-
-const CheckScriptAssets = (assets: string) => {
-    if (!JS_ASSETS_LIST.includes(assets)) {
-        JS_ASSETS_LIST.push(assets);
-        return true;
-    } else return false;
-};
-
 const loadScript = (url: string) => {
-    if (!CheckScriptAssets(url)) {
-        if (document.getElementById(url)) document?.getElementById(url)?.remove();
-    }
+    document?.getElementById('tmp-script')?.remove();
     return new Promise((resolve, reject) => {
         const sScript = document.createElement('script');
         sScript.src = url;
-        sScript.id = url;
+        sScript.id = 'tmp-script';
         sScript.type = 'text/javascript';
         sScript.onload = resolve;
         sScript.onerror = reject;
--- src/components/chart/index.tsx
+++ src/components/chart/index.tsx
@@ -1,77 +1,51 @@
 import { getTqlChart } from '@/api/repository/machiot';
 import { useRef, useState, useEffect } from 'react';
-import { reSizeChart, drawChart } from '@/plugin/eCharts';
 import { sqlBasicChartFormatter } from '@/utils/sqlFormatter';
-import useDebounce from '@/hooks/useDebounce';
 import { Play } from '@/assets/icons/Icon';
 import './index.scss';
+import { loadScriptsSequentially } from '@/assets/ts/ScriptRegister';
 
 const CHART = ({
     pQueryList,
     pChartAixsList,
-    pIsVertical,
     pDisplay,
     pSqlQueryTxt,
-    pSizes,
 }: {
     pQueryList: string[] | [];
     pChartAixsList: string[];
-    pIsVertical: boolean;
+    pIsVertical?: boolean;
     pDisplay: string;
     pSqlQueryTxt: () => string;
-    pSizes: number[] | string[];
+    pSizes?: number[] | string[];
 }) => {
     const [sResult, setResult] = useState<any>(null);
     const [sStyle, setStyle] = useState({ width: 0, height: 0 });
-    const [sChartDiv, setChartDiv] = useState<any>(null);
     const [sSelectedXAxis, setSelectedXAxis] = useState<string>('');
     const [sSelectedYAxis, setSelectedYAxis] = useState<string>('');
-    const [sTheme, setTheme] = useState<string>('dark');
     const chartRef = useRef<any>(null);
     const sControlPanelHeight: number = 40;
     const sControlList: string[] = ['X Axis', 'Y Axis'];
 
-    const getChartData = async (aSize: any) => {
+    const getChartData = async () => {
         if (pChartAixsList.length === 0) return;
-        let sTmpResult: any = '';
-        if (sSelectedXAxis && sSelectedYAxis)
-            sTmpResult = await getTqlChart(
-                sqlBasicChartFormatter(pSqlQueryTxt(), aSize.width, aSize.height, {
-                    x: sSelectedXAxis,
-                    y: sSelectedYAxis,
-                    xIndex: pChartAixsList.indexOf(sSelectedXAxis),
-                    yIndex: pChartAixsList.indexOf(sSelectedYAxis),
-                })
-            );
-        else sTmpResult = await getTqlChart(sqlBasicChartFormatter(pSqlQueryTxt(), aSize.width, aSize.height));
-        const sIsData = !!sTmpResult.data.chartOption.series;
-        if (sIsData) {
-            const sTheme = sTmpResult.data.theme && sTmpResult.data.theme !== '-' ? sTmpResult.data.theme : 'white';
-            setTheme(sTheme);
-            const sDataLength = sTmpResult.data.chartOption.series.length > 0 ? sTmpResult.data.chartOption.series[0].data.length : 0;
-            sTmpResult.data.chartOption.dataZoom[0].start = 100 - (5 * aSize.width) / sDataLength;
-            sTmpResult.data.chartOption.dataZoom[0].end = 100;
-
-            setResult(sTmpResult.data);
-
-            setTimeout(() => {
-                const tmpChartDiv = drawChart(sTmpResult.data, sTheme);
-                setChartDiv(tmpChartDiv);
-            }, 100);
-        } else setResult(null);
+        const tmpSize = { width: Math.floor(chartRef.current.clientWidth), height: Math.floor(chartRef.current.clientHeight - sControlPanelHeight) };
+        setStyle(tmpSize);
+        const sTmpResult = await getTqlChart(
+            sqlBasicChartFormatter(pSqlQueryTxt(), {
+                x: sSelectedXAxis,
+                y: sSelectedYAxis,
+                xIndex: pChartAixsList.indexOf(sSelectedXAxis),
+                yIndex: pChartAixsList.indexOf(sSelectedYAxis),
+                list: pChartAixsList,
+            })
+        );
+        setResult(sTmpResult.data);
+        if (sTmpResult.data && sTmpResult.data.jsAssets) await loadScriptsSequentially(sTmpResult.data.jsAssets);
+        if (sTmpResult.data && sTmpResult.data.jsCodeAssets) await loadScriptsSequentially(sTmpResult.data.jsCodeAssets);
     };
 
     useEffect(() => {
-        if (chartRef && chartRef.current && !!pSqlQueryTxt() && pQueryList.length > 0) {
-            getChartData({
-                width: chartRef.current.clientWidth,
-                height: chartRef.current.clientHeight - sControlPanelHeight,
-            });
-            setStyle({
-                width: Math.floor(chartRef.current.clientWidth),
-                height: Math.floor(chartRef.current.clientHeight - sControlPanelHeight),
-            });
-        }
+        if (chartRef && chartRef.current && !!pSqlQueryTxt() && pQueryList.length > 0) getChartData();
     }, [pDisplay]);
 
     useEffect(() => {
@@ -81,25 +55,6 @@ const CHART = ({
         }
     }, [pChartAixsList]);
 
-    const reDrawChart = () => {
-        if (!sResult) return;
-        const sTmpChartDiv: any = sChartDiv;
-        const sDataLength = sResult.chartOption.series && sResult.chartOption.series.length > 0 ? sResult.chartOption.series[0].data.length : 0;
-        sTmpChartDiv._model.option.dataZoom[0].start = 100 - (5 * chartRef.current.clientWidth) / sDataLength;
-        sTmpChartDiv._model.option.dataZoom[0].end = 100;
-
-        setChartDiv(sTmpChartDiv);
-
-        setStyle({
-            width: Math.floor(chartRef.current.clientWidth),
-            height: Math.floor(chartRef.current.clientHeight - sControlPanelHeight),
-        });
-        reSizeChart(sChartDiv, {
-            width: Math.floor(chartRef.current.clientWidth) + 'px',
-            height: Math.floor(chartRef.current.clientHeight - sControlPanelHeight) + 'px',
-        });
-    };
-
     const handleAxis = (e: any, aControl: string) => {
         switch (aControl) {
             case 'X Axis':
@@ -108,8 +63,6 @@ const CHART = ({
                 return setSelectedYAxis(e.target.value);
         }
     };
-
-    useDebounce([pSizes, pIsVertical], reDrawChart);
 
     return pDisplay === '' ? (
         <div ref={chartRef} className="chart-wrapper">
@@ -141,18 +94,12 @@ const CHART = ({
                             );
                         })}
                         <div className="chart-control-play-btn">
-                            <Play className="chart-control-play-btn-item" size="20px" color="#939498" onClick={() => getChartData(sStyle)} />
+                            <Play className="chart-control-play-btn-item" size="20px" color="#939498" onClick={getChartData} />
                         </div>
                     </div>
-                    {sResult && (
-                        <div className="chart_container">
-                            <div
-                                className="chart_item"
-                                id={sResult.chartID}
-                                style={{ backgroundColor: sTheme === 'dark' ? '' : '#FFF', width: sStyle.width + 'px', height: sStyle.height + 'px' }}
-                            />
-                        </div>
-                    )}
+                    <div className="chart_container">
+                        <div className="chart_item" id={sResult.chartID} style={{ width: sStyle.width + 'px', height: sStyle.height + 'px' }} />
+                    </div>
                 </>
             ) : null}
         </div>
