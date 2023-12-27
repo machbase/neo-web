72d678978976034c41700807ae846214ee0d3344
kevin-lee <kevin.lee@machbase.com>
2023-12-27 14:22:41 +0900

    machbase/neo-web#602 - fix chart dom

--- src/utils/UncaughtErrorHelper.tsx
+++ src/utils/UncaughtErrorHelper.tsx
@@ -1,4 +1,7 @@
 export const UncaughtErrorObserver = (setConsoleList: any) => {
+    const sWarnTmp = window.console.warn;
+    const sErrorTmp = window.console.error;
+    // const sLogTmp = window.console.log;
     window.onerror = function (message) {
         setConsoleList((preData: any) => [
             ...preData,
@@ -11,4 +14,40 @@ export const UncaughtErrorObserver = (setConsoleList: any) => {
         ]);
         return true;
     };
+    window.console.warn = (message, loc) => {
+        sWarnTmp(message, loc);
+        setConsoleList((preData: any) => [
+            ...preData,
+            {
+                timestamp: new Date().getTime(),
+                level: 'WARN',
+                task: '',
+                message: `${message} ${loc}`,
+            },
+        ]);
+    };
+    window.console.error = (message, loc) => {
+        sErrorTmp(message, loc);
+        setConsoleList((preData: any) => [
+            ...preData,
+            {
+                timestamp: new Date().getTime(),
+                level: 'ERROR',
+                task: '',
+                message: `${message} ${loc}`,
+            },
+        ]);
+    };
+    // window.console.log = (message, loc) => {
+    //     sLogTmp(message, loc);
+    //     setConsoleList((preData: any) => [
+    //         ...preData,
+    //         {
+    //             timestamp: new Date().getTime(),
+    //             level: 'INFO',
+    //             task: '',
+    //             message: `${message} ${loc}`,
+    //         },
+    //     ]);
+    // };
 };
