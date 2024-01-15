74f3ee1321a4469b9589f0207fa8891584c42183
jeff.park <jeff.park@machbase.com>
2024-01-15 17:03:24 +0900

    machbase/neo#597 feat gauge options

---
+++ src/components/dashboard/createPanel/option/GaugeOptions.tsx
@@ -0,0 +1,121 @@
+import { Collapse } from '@/components/collapse/Collapse';
+import CheckBox from '@/components/inputs/CheckBox';
+import { Input } from '@/components/inputs/Input';
+
+interface GaugeOptionProps {
+    pPanelOption: any;
+    pSetPanelOption: any;
+}
+
+export const GaugeOptions = (props: GaugeOptionProps) => {
+    const { pPanelOption, pSetPanelOption } = props;
+
+    const sMenuStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginRight: '10px',
+        marginBottom: '5px',
+    };
+
+    const handleGaugeOption = (aValue: string | boolean, aKey: string) => {
+        pSetPanelOption((aPrev: any) => {
+            return {
+                ...aPrev,
+                chartOptions: {
+                    ...aPrev.chartOptions,
+                    [aKey]: aValue,
+                },
+            };
+        });
+    };
+
+    return (
+        <div>
+            <div style={sMenuStyle}>
+                <div>Min</div>
+                <Input
+                    pType="number"
+                    pHeight={25}
+                    pWidth={50}
+                    pBorderRadius={4}
+                    pValue={pPanelOption.chartOptions?.min}
+                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'min')}
+                />
+            </div>
+            <div style={sMenuStyle}>
+                <div>Max</div>
+                <Input
+                    pType="number"
+                    pHeight={25}
+                    pWidth={50}
+                    pBorderRadius={4}
+                    pValue={pPanelOption.chartOptions?.max}
+                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'max')}
+                />
+            </div>
+            <CheckBox
+                pText="Show Axis Tick"
+                pDefaultChecked={pPanelOption.chartOptions?.isAxisTick ?? false}
+                onChange={(aEvent: any) => handleGaugeOption(aEvent.target.checked, 'isAxisTick')}
+            />
+            <div style={{ height: '10px' }} />
+            <div style={sMenuStyle}>
+                <div>Axis Label Distance</div>
+                <Input
+                    pType="number"
+                    pHeight={25}
+                    pWidth={50}
+                    pBorderRadius={4}
+                    pValue={pPanelOption.chartOptions?.axisLabelDistance}
+                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'axisLabelDistance')}
+                />
+            </div>
+            <CheckBox
+                pText="Show Anchor"
+                pDefaultChecked={pPanelOption.chartOptions?.isAnchor ?? false}
+                onChange={(aEvent: any) => handleGaugeOption(aEvent.target.checked, 'isAnchor')}
+            />
+            <div style={{ height: '10px' }} />
+            <div style={sMenuStyle}>
+                <div>Anchor Size</div>
+                <Input
+                    pType="number"
+                    pHeight={25}
+                    pWidth={50}
+                    pBorderRadius={4}
+                    pIsDisabled={!pPanelOption.chartOptions?.isAnchor}
+                    pValue={pPanelOption.chartOptions?.anchorSize}
+                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'anchorSize')}
+                />
+            </div>
+            <div style={sMenuStyle}>
+                <div>Font Size</div>
+                <Input
+                    pType="number"
+                    pHeight={25}
+                    pWidth={50}
+                    pBorderRadius={4}
+                    pValue={pPanelOption.chartOptions?.valueFontSize}
+                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'valueFontSize')}
+                />
+            </div>
+            <CheckBox
+                pText="Active Animation"
+                pDefaultChecked={pPanelOption.chartOptions?.valueAnimation ?? false}
+                onChange={(aEvent: any) => handleGaugeOption(aEvent.target.checked, 'valueAnimation')}
+            />
+            <div style={sMenuStyle}>
+                <div>Value Center Offset</div>
+                <Input
+                    pType="number"
+                    pHeight={25}
+                    pWidth={50}
+                    pBorderRadius={4}
+                    pValue={pPanelOption.chartOptions?.alignCenter}
+                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'alignCenter')}
+                />
+            </div>
+        </div>
+    );
+};
