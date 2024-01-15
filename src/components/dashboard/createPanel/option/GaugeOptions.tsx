9817ff801c73a46231d72557f227dd8d219fcd88
jeff.park <jeff.park@machbase.com>
2024-01-15 17:10:14 +0900

    machbase/neo#597 fix gauge options

--- src/components/dashboard/createPanel/option/GaugeOptions.tsx
+++ src/components/dashboard/createPanel/option/GaugeOptions.tsx
@@ -54,68 +54,77 @@ export const GaugeOptions = (props: GaugeOptionProps) => {
                     onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'max')}
                 />
             </div>
-            <CheckBox
-                pText="Show Axis Tick"
-                pDefaultChecked={pPanelOption.chartOptions?.isAxisTick ?? false}
-                onChange={(aEvent: any) => handleGaugeOption(aEvent.target.checked, 'isAxisTick')}
-            />
-            <div style={{ height: '10px' }} />
-            <div style={sMenuStyle}>
-                <div>Axis Label Distance</div>
-                <Input
-                    pType="number"
-                    pHeight={25}
-                    pWidth={50}
-                    pBorderRadius={4}
-                    pValue={pPanelOption.chartOptions?.axisLabelDistance}
-                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'axisLabelDistance')}
+            <div className="divider" />
+            <Collapse title="Axis">
+                <CheckBox
+                    pText="Show Axis Tick"
+                    pDefaultChecked={pPanelOption.chartOptions?.isAxisTick ?? false}
+                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.checked, 'isAxisTick')}
                 />
-            </div>
-            <CheckBox
-                pText="Show Anchor"
-                pDefaultChecked={pPanelOption.chartOptions?.isAnchor ?? false}
-                onChange={(aEvent: any) => handleGaugeOption(aEvent.target.checked, 'isAnchor')}
-            />
-            <div style={{ height: '10px' }} />
-            <div style={sMenuStyle}>
-                <div>Anchor Size</div>
-                <Input
-                    pType="number"
-                    pHeight={25}
-                    pWidth={50}
-                    pBorderRadius={4}
-                    pIsDisabled={!pPanelOption.chartOptions?.isAnchor}
-                    pValue={pPanelOption.chartOptions?.anchorSize}
-                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'anchorSize')}
+                <div style={{ height: '10px' }} />
+                <div style={sMenuStyle}>
+                    <div>Axis Label Distance</div>
+                    <Input
+                        pType="number"
+                        pHeight={25}
+                        pWidth={50}
+                        pBorderRadius={4}
+                        pValue={pPanelOption.chartOptions?.axisLabelDistance}
+                        onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'axisLabelDistance')}
+                    />
+                </div>
+            </Collapse>
+            <div className="divider" />
+            <Collapse title="Anchor">
+                <CheckBox
+                    pText="Show Anchor"
+                    pDefaultChecked={pPanelOption.chartOptions?.isAnchor ?? false}
+                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.checked, 'isAnchor')}
                 />
-            </div>
-            <div style={sMenuStyle}>
-                <div>Font Size</div>
-                <Input
-                    pType="number"
-                    pHeight={25}
-                    pWidth={50}
-                    pBorderRadius={4}
-                    pValue={pPanelOption.chartOptions?.valueFontSize}
-                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'valueFontSize')}
+                <div style={{ height: '10px' }} />
+                <div style={sMenuStyle}>
+                    <div>Anchor Size</div>
+                    <Input
+                        pType="number"
+                        pHeight={25}
+                        pWidth={50}
+                        pBorderRadius={4}
+                        pIsDisabled={!pPanelOption.chartOptions?.isAnchor}
+                        pValue={pPanelOption.chartOptions?.anchorSize}
+                        onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'anchorSize')}
+                    />
+                </div>
+            </Collapse>
+            <div className="divider" />
+            <Collapse title="Value">
+                <div style={sMenuStyle}>
+                    <div>Font Size</div>
+                    <Input
+                        pType="number"
+                        pHeight={25}
+                        pWidth={50}
+                        pBorderRadius={4}
+                        pValue={pPanelOption.chartOptions?.valueFontSize}
+                        onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'valueFontSize')}
+                    />
+                </div>
+                <div style={sMenuStyle}>
+                    <div>Value Center Offset</div>
+                    <Input
+                        pType="number"
+                        pHeight={25}
+                        pWidth={50}
+                        pBorderRadius={4}
+                        pValue={pPanelOption.chartOptions?.alignCenter}
+                        onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'alignCenter')}
+                    />
+                </div>
+                <CheckBox
+                    pText="Active Animation"
+                    pDefaultChecked={pPanelOption.chartOptions?.valueAnimation ?? false}
+                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.checked, 'valueAnimation')}
                 />
-            </div>
-            <CheckBox
-                pText="Active Animation"
-                pDefaultChecked={pPanelOption.chartOptions?.valueAnimation ?? false}
-                onChange={(aEvent: any) => handleGaugeOption(aEvent.target.checked, 'valueAnimation')}
-            />
-            <div style={sMenuStyle}>
-                <div>Value Center Offset</div>
-                <Input
-                    pType="number"
-                    pHeight={25}
-                    pWidth={50}
-                    pBorderRadius={4}
-                    pValue={pPanelOption.chartOptions?.alignCenter}
-                    onChange={(aEvent: any) => handleGaugeOption(aEvent.target.value, 'alignCenter')}
-                />
-            </div>
+            </Collapse>
         </div>
     );
 };
