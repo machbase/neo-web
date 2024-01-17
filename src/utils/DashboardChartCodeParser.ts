f86c6a738d2226d56a2eba58b23ff4612e60ee02
kevin-lee <kevin.lee@machbase.com>
2024-01-17 16:43:09 +0900

    update parser

--- src/utils/DashboardChartCodeParser.ts
+++ src/utils/DashboardChartCodeParser.ts
@@ -1,28 +1,35 @@
-export const DashboardChartCodeParser = async (aParsedQuery: any, aTake: number) => {
+export const DashboardChartCodeParser = async (aParsedQuery: any) => {
     // GEN variable
     const sDynamicVariable = aParsedQuery.map((aQuery: any) => {
         return {
-            q: `SQL("${aQuery.sql}")\nTAKE(${aTake})\nJSON()`,
-            i: aQuery.idx,
+            query: `${aQuery.query}`,
+            idx: aQuery.idx,
+            type: aQuery.dataType,
         };
     });
     // GEN func
-    const sFunction = `function getData(aTql, aIdx) {
+    const sFunction = `function getData(aTql, aIdx, aType) {
         fetch("http://${window.location.hostname}:5654/db/tql", {
             method: "POST",
             body: aTql
-        }).then(function(rsp){
-            return rsp.json()
-        }).then(function(obj){
-            _chartOption.series[aIdx].data = obj.data.rows
-            _chart.setOption(_chartOption)
-        }).catch(function(err){
-            console.warn("data fetch error", err)
-        });
+        })
+        .then((rsp) => rsp.json())
+        .then((obj) => {
+            if (aType === 'TIME_VALUE') {
+                _chartOption.series[aIdx].data = obj.data.rows;
+            } else if (aType === 'NAME_VALUE') {
+                let aDatas = [];
+                obj.data.rows.forEach((aData)=>{
+                    aDatas.push(aData[0])
+                });
+                _chartOption.series[0].data = [..._chartOption.series[0].data, ...aDatas];
+            }
+            _chart.setOption(_chartOption);
+        }).catch((err) => console.warn("data fetch error", err));
     };`;
     // GEN loop
     const sLoop = `sDatas.forEach((aData)=>{
-        getData(aData.q, aData.i);
+        getData(aData.query, aData.idx, aData.type);
     })`;
 
     return `{
