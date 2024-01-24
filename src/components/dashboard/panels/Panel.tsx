84027bebf14c1904ced3d2ae1c56b70e28d31ab4
jeff.park <jeff.park@machbase.com>
2024-01-24 14:16:10 +0900

    machbase/neo#597 feat panel header add context menu

--- src/components/dashboard/panels/Panel.tsx
+++ src/components/dashboard/panels/Panel.tsx
@@ -2,7 +2,6 @@ import LineChart from './chart/LineChart';
 import PanelHeader from './PanelHeader';
 import './Panel.scss';
 import { useState } from 'react';
-import { GoGrabber } from '@/assets/icons/Icon';
 import { ChartThemeBackgroundColor } from '@/utils/constants';
 
 const Panel = ({ pBoardInfo, pShowEditPanel, pType, pPanelInfo, pInsetDraging, pDragStat, pIsView, pModifyState, pSetModifyState, pParentWidth, pIsHeader }: any) => {
@@ -10,21 +9,16 @@ const Panel = ({ pBoardInfo, pShowEditPanel, pType, pPanelInfo, pInsetDraging, p
 
     return (
         <div className="panel-wrap" style={{ backgroundColor: ChartThemeBackgroundColor[pPanelInfo.theme] }}>
-            {pIsHeader ? (
-                <PanelHeader
-                    pRefreshCount={sRefreshCount}
-                    pSetRefreshCount={setRefreshCount}
-                    pShowEditPanel={pShowEditPanel}
-                    pType={pType}
-                    pBoardInfo={pBoardInfo}
-                    pPanelInfo={pPanelInfo}
-                    pIsView={pIsView}
-                ></PanelHeader>
-            ) : pType === undefined ? (
-                <div className="draggable-panel-header">
-                    <GoGrabber size={20} color={pPanelInfo.theme !== 'dark' ? 'black' : ''} />
-                </div>
-            ) : null}
+            <PanelHeader
+                pRefreshCount={sRefreshCount}
+                pSetRefreshCount={setRefreshCount}
+                pShowEditPanel={pShowEditPanel}
+                pType={pType}
+                pBoardInfo={pBoardInfo}
+                pPanelInfo={pPanelInfo}
+                pIsView={pIsView}
+                pIsHeader={pIsHeader}
+            ></PanelHeader>
             {pPanelInfo && (
                 <LineChart
                     pDragStat={pDragStat}
--- src/components/dashboard/panels/PanelHeader.tsx
+++ src/components/dashboard/panels/PanelHeader.tsx
@@ -1,17 +1,20 @@
-import { Delete, GearFill, VscRecord } from '@/assets/icons/Icon';
+import { Delete, GearFill, VscRecord, GoGrabber } from '@/assets/icons/Icon';
 import { IconButton } from '@/components/buttons/IconButton';
 import { gBoardList, GBoardListType, gSelectedTab } from '@/recoil/recoil';
 import { useRecoilState } from 'recoil';
-import { useState } from 'react';
 import './PanelHeader.scss';
 import { Tooltip } from 'react-tooltip';
 import { generateRandomString } from '@/utils';
+import Menu from '@/components/contextMenu/Menu';
+import { useState, useRef } from 'react';
+import useOutsideClick from '@/hooks/useOutsideClick';
 
-const PanelHeader = ({ pShowEditPanel, pType, pPanelInfo, pIsView }: any) => {
+const PanelHeader = ({ pShowEditPanel, pType, pPanelInfo, pIsView, pIsHeader }: any) => {
+    const [sIsContextMenu, setIsContextMenu] = useState<boolean>(false);
     const [sBoardList, setBoardList] = useRecoilState<GBoardListType[]>(gBoardList);
-    const [sMouseDown, setMouseDown] = useState(false);
     const [sSelectedTab] = useRecoilState(gSelectedTab);
     const sHeaderId = generateRandomString();
+    const sMenuRef = useRef<HTMLDivElement>(null);
 
     const removePanel = () => {
         setBoardList(
@@ -29,69 +32,67 @@ const PanelHeader = ({ pShowEditPanel, pType, pPanelInfo, pIsView }: any) => {
         );
     };
 
+    const handleContextMenu = (aEvent: React.MouseEvent) => {
+        aEvent.preventDefault();
+        setIsContextMenu(true);
+    };
+
+    const handleDeleteOnMenu = (aEvent: React.MouseEvent) => {
+        aEvent.stopPropagation();
+        removePanel();
+        setIsContextMenu(false);
+    };
+
+    const handleMoveEditOnMenu = (aEvent: React.MouseEvent, aPanelId: string) => {
+        aEvent.stopPropagation();
+        pShowEditPanel('edit', aPanelId);
+        setIsContextMenu(false);
+    };
+
+    useOutsideClick(sMenuRef, () => setIsContextMenu(false));
+
     return (
         <>
+            <div ref={sMenuRef} className={`draggable-panel-header ${pIsHeader || pType !== undefined ? 'display-none' : ''}`}>
+                <GoGrabber size={20} color={pPanelInfo.theme !== 'dark' ? 'black' : ''} onContextMenu={handleContextMenu} />
+                <div className="hidden-header-menu">
+                    <Menu isOpen={sIsContextMenu}>
+                        <Menu.Item onClick={(aEvent: any) => handleMoveEditOnMenu(aEvent, pPanelInfo.id)}>
+                            <GearFill />
+                            <span>Setting</span>
+                        </Menu.Item>
+                        <Menu.Item onClick={handleDeleteOnMenu}>
+                            <Delete />
+                            <span>Delete</span>
+                        </Menu.Item>
+                    </Menu>
+                </div>
+            </div>
             <div
-                onMouseDown={() => setMouseDown(true)}
-                onMouseUp={() => setMouseDown(false)}
-                style={
-                    pType !== 'create' && pType !== 'edit'
-                        ? !sMouseDown
-                            ? {
-                                  cursor: 'grab',
-                              }
-                            : {
-                                  cursor: 'grabbing',
-                              }
-                        : {}
-                }
-                className={`board-panel-header ${pPanelInfo.theme !== 'dark' ? 'panel-theme-white' : ''}`}
+                className={`board-panel-header ${!pIsHeader ? 'display-none' : ''} ${pPanelInfo.theme !== 'dark' ? 'panel-theme-white' : ''} ${
+                    pType === undefined ? 'cursor-grab' : ''
+                }`}
             >
                 <div>{pPanelInfo.title}</div>
-                <div className="panel-header-navigator">
-                    {pPanelInfo.useCustomTime && pType === undefined && (
-                        <>
-                            <a data-tooltip-place="bottom" className="panel-header-time-range" id={sHeaderId}>
-                                <VscRecord color="#339900" />
-                            </a>
-                            <Tooltip
-                                className="tooltip"
-                                anchorSelect={'#' + sHeaderId}
-                                content={`${pPanelInfo.timeRange.start} ~ ${pPanelInfo.timeRange.end} , ${pPanelInfo.timeRange.refresh}`}
-                            />
-                        </>
-                    )}
-
-                    {/* <span className="delete">
-                        <IconButton
-                            pDisabled={pIsView}
-                            pWidth={25}
-                            pIcon={<Refresh size={14} />}
-                            onClick={() =>
-                                pSetRefreshCount((Prev: any) => {
-                                    return Prev + 1;
-                                })
-                            }
+                <div className={`panel-header-navigator ${pType !== undefined ? 'display-none' : ''}`}>
+                    <a data-tooltip-place="bottom" className={`panel-header-time-range ${!pPanelInfo.useCustomTime ? 'display-none' : ''}`} id={sHeaderId}>
+                        <VscRecord color="#339900" />
+                        <Tooltip
+                            className="tooltip"
+                            anchorSelect={'#' + sHeaderId}
+                            content={`${pPanelInfo.timeRange.start} ~ ${pPanelInfo.timeRange.end} , ${pPanelInfo.timeRange.refresh}`}
                         />
-                    </span> */}
-                    {pType !== 'create' && pType !== 'edit' && (
-                        <>
-                            <span className="delete">
-                                <IconButton
-                                    pDisabled={pIsView}
-                                    pWidth={25}
-                                    pIcon={<GearFill size={14} />}
-                                    onClick={(aEvent: any) => {
-                                        aEvent.stopPropagation();
-                                        pShowEditPanel('edit', pPanelInfo.id);
-                                    }}
-                                />
-                            </span>
-                            <span className="delete">
-                                <IconButton pDisabled={pIsView} pWidth={25} pIcon={<Delete size={18} />} onClick={() => removePanel()} />
-                            </span>
-                        </>
-                    )}
+                    </a>
+                    <IconButton
+                        pDisabled={pIsView}
+                        pWidth={25}
+                        pIcon={<GearFill size={14} />}
+                        onClick={(aEvent: any) => {
+                            aEvent.stopPropagation();
+                            pShowEditPanel('edit', pPanelInfo.id);
+                        }}
+                    />
+                    <IconButton pDisabled={pIsView} pWidth={25} pIcon={<Delete size={18} />} onClick={() => removePanel()} />
                 </div>
             </div>
         </>
