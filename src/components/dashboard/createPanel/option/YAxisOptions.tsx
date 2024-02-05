b3b59208fe80fe2b1094e862d43a470561de9c75
kevin-lee <kevin.lee@machbase.com>
2024-02-05 17:51:43 +0900

    machbase/neo-web#597 - fix yAxis min max

--- src/components/dashboard/createPanel/option/YAxisOptions.tsx
+++ src/components/dashboard/createPanel/option/YAxisOptions.tsx
@@ -1,5 +1,6 @@
 // import { TextButton } from '@/components/buttons/TextButton';
 import { Collapse } from '@/components/collapse/Collapse';
+import CheckBox from '@/components/inputs/CheckBox';
 import { Input } from '@/components/inputs/Input';
 import { Select } from '@/components/inputs/Select';
 // import { DefaultYAxisOption } from '@/utils/eChartHelper';
@@ -53,37 +54,46 @@ export const YAxisOptions = (props: XAxisOptionProps) => {
     return (
         <Collapse title="yAxis">
             {pYAxis.map((aItem: any, aIndex: number) => (
-                <div key={aItem.type + aIndex} className="menu-style">
-                    <div>Position</div>
-                    <Select
-                        pWidth={100}
-                        pHeight={25}
-                        pBorderRadius={4}
-                        pInitValue={aItem.position}
-                        onChange={(aEvent) => handleYAxisPosition(aEvent, aIndex)}
-                        pOptions={sPositionList}
-                    />
-                    {/* <div className="divider" /> */}
-                    <div>Min</div>
-                    <Input
-                        pType="number"
-                        pWidth={50}
-                        pHeight={25}
-                        pBorderRadius={4}
-                        pIsDisabled={false}
-                        pValue={aItem?.min ?? 0}
-                        onChange={(aEvent: any) => HandleMinMax('min', aEvent.target.value, aIndex)}
-                    />
-                    <div>Max</div>
-                    <Input
-                        pType="number"
-                        pWidth={50}
-                        pHeight={25}
-                        pBorderRadius={4}
-                        pIsDisabled={false}
-                        pValue={aItem?.max ?? 0}
-                        onChange={(aEvent: any) => HandleMinMax('max', aEvent.target.value, aIndex)}
-                    />
+                <div key={aItem.type + aIndex}>
+                    <div className="menu-style">
+                        <div>Position</div>
+                        <Select
+                            pWidth={100}
+                            pHeight={25}
+                            pBorderRadius={4}
+                            pInitValue={aItem.position}
+                            onChange={(aEvent) => handleYAxisPosition(aEvent, aIndex)}
+                            pOptions={sPositionList}
+                        />
+                    </div>
+                    <div className="divider" />
+                    <div className="menu-style">
+                        <CheckBox
+                            pText="Use min max"
+                            pDefaultChecked={aItem.useMinMax ?? false}
+                            onChange={(aEvent: any) => HandleMinMax('useMinMax', aEvent.target.checked, aIndex)}
+                        />
+                        <div>Min</div>
+                        <Input
+                            pType="number"
+                            pWidth={50}
+                            pHeight={25}
+                            pBorderRadius={4}
+                            pIsDisabled={!aItem.useMinMax}
+                            pValue={aItem?.min ?? 0}
+                            onChange={(aEvent: any) => HandleMinMax('min', aEvent.target.value, aIndex)}
+                        />
+                        <div>Max</div>
+                        <Input
+                            pType="number"
+                            pWidth={50}
+                            pHeight={25}
+                            pBorderRadius={4}
+                            pIsDisabled={!aItem.useMinMax}
+                            pValue={aItem?.max ?? 0}
+                            onChange={(aEvent: any) => HandleMinMax('max', aEvent.target.value, aIndex)}
+                        />
+                    </div>
                 </div>
             ))}
 
