c8750aa742a506c4677bd2a47c560055c1c241a2
kevin-lee <kevin.lee@machbase.com>
2024-05-20 10:10:46 +0900

    machbase/neo-web#755 - fix build err2

--- src/view/Login/Login.test.tsx
+++ src/view/Login/Login.test.tsx
@@ -1,3 +1,10 @@
+/**
+ * ReferenceError: TextEncoder is not defined
+ * https://stackoverflow.com/questions/68468203/why-am-i-getting-textencoder-is-not-defined-in-jest
+ */
+import { TextEncoder, TextDecoder } from 'util';
+Object.assign(global, { TextDecoder, TextEncoder });
+
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'; //toBeInTheDocument() 를 사용하기 위해서 필요합니다!
 import Login from './Login';
