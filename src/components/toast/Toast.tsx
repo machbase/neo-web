bca9e3b4471a529ec45eec878d1bb07e79ef37f5
kevin-lee <kevin.lee@machbase.com>
2024-07-10 17:49:11 +0900

    machbase/neo-web#818 - update toast

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,4 +1,6 @@
 import toast from 'react-hot-toast';
+import { VscClose } from 'react-icons/vsc';
+import './Toast.scss';
 
 const toastStyle = {
     backgroundColor: '#404457',
@@ -7,9 +9,17 @@ const toastStyle = {
 };
 
 export const Error = (aMessage: string) => {
-    return toast.error(aMessage, {
-        style: toastStyle,
-    });
+    return toast(
+        (t) => (
+            <div className="toast-custom-style-error">
+                <div className="toast-custom-icon-error" onClick={() => toast.remove(t.id)}>
+                    <VscClose />
+                </div>
+                <div className="toast-custom-text-error"> {aMessage} </div>
+            </div>
+        ),
+        { className: 'toast-custom-outside' }
+    );
 };
 
 export const Success = (aMessage: string) => {
