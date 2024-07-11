71ea21f210912ea7e306dcff1f5373b2fe1e8c4c
kevin-lee <kevin.lee@machbase.com>
2024-07-11 10:48:04 +0900

    machbase/neo-web#818 - update toast close (success)

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,21 +1,15 @@
 import toast from 'react-hot-toast';
-import { VscClose } from 'react-icons/vsc';
+import { VscCheck, VscClose } from 'react-icons/vsc';
 import './Toast.scss';
-
-const toastStyle = {
-    backgroundColor: '#404457',
-    color: 'white',
-    border: '0.5px solid rgba(255, 255, 255, 0.5)',
-};
 
 export const Error = (aMessage: string) => {
     return toast(
         (t) => (
-            <div className="toast-custom-style-error">
+            <div className="toast-custom-style">
                 <div className="toast-custom-icon-error" onClick={() => toast.remove(t.id)}>
                     <VscClose />
                 </div>
-                <div className="toast-custom-text-error"> {aMessage} </div>
+                <div className="toast-custom-text"> {aMessage} </div>
             </div>
         ),
         { className: 'toast-custom-outside' }
@@ -23,7 +17,15 @@ export const Error = (aMessage: string) => {
 };
 
 export const Success = (aMessage: string) => {
-    return toast.success(aMessage, {
-        style: toastStyle,
-    });
+    return toast(
+        (t) => (
+            <div className="toast-custom-style">
+                <div className="toast-custom-icon-success" onClick={() => toast.remove(t.id)}>
+                    <VscCheck />
+                </div>
+                <div className="toast-custom-text"> {aMessage} </div>
+            </div>
+        ),
+        { className: 'toast-custom-outside' }
+    );
 };
