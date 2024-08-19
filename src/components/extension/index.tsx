f255a0b43b2663cc9e54473a3d76520e9d374f3f
kevin-lee <kevin.lee@machbase.com>
2024-08-19 13:39:21 +0900

    machbase/neo-web#836 - fix experiment mode

--- src/components/extension/index.tsx
+++ src/components/extension/index.tsx
@@ -146,7 +146,7 @@ const Extension = ({ pHandleSideBar, pSetSideSizes, pIsSidebar }: any) => {
                         sExtensionList.length !== 0 &&
                         sExtensionList.map((aItem: any, aIdx: number) => {
                             return (
-                                !(localStorage.getItem('experimentMode') && aItem.label === 'APPSTORE') && (
+                                ((localStorage.getItem('experimentMode') === 'true' && aItem.label === 'APPSTORE') || aItem.label !== 'APPSTORE') && (
                                     <div key={aIdx} className={`extension-top-list-item`} onClick={() => selectExtension(aItem)}>
                                         <ExtensionBtn pLabel={aItem.label} pIcon={setIcon(aItem.id)} />
                                     </div>
