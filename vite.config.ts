89ce96f9f589226b377acb4820470ed483a69ca4
kevin-lee <kevin.lee@machbase.com>
2024-10-11 12:36:51 +0900

    fix build err

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+    plugins: [react(), svgr()],
+    resolve: {
+        alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+    build: {
+        chunkSizeWarningLimit: 10000,
+        target: 'es2020',
+    },
+    base: '/web/ui',
+});
