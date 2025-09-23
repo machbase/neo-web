502e992ce5a340c1efa5a253012b75d09d8cb38e
max-kim <max.kim@machbase.com>
2025-09-23 16:09:40 +0900

    [FIX] Resolve TQL variable replacement with empty variable arrays

--- src/public-dashboard/components/variable/index.ts
+++ src/public-dashboard/components/variable/index.ts
@@ -14,5 +14,27 @@ export interface VARIABLE_TYPE {
 
 export type VARIABLE_LIST_TYPE = VARIABLE_TYPE[];
 
-// Default variable list
-export const DEFAULT_VARIABLE_LIST: VARIABLE_LIST_TYPE = [];
+export enum VARIABLE_DEFAULT_TYPE {
+    DEFAULT_DEFINED = 'DEFAULT_DEFINED',
+}
+const defaultVariableList: string[] = ['from_str', 'from_s', 'from_ms', 'from_us', 'from_ns', 'to_str', 'to_s', 'to_ms', 'to_us', 'to_ns', 'period', 'period_unit', 'period_value'];
+export const DEFAULT_VARIABLE_LIST: VARIABLE_TYPE[] = defaultVariableList.map((defaultVariable) => {
+    return {
+        id: `default_variable-${defaultVariable}`,
+        label: defaultVariable,
+        key: `{{${defaultVariable}}}`,
+        type: 'SELECT',
+        use: {
+            id: `default_variable-${defaultVariable}-value`,
+            type: VARIABLE_DEFAULT_TYPE.DEFAULT_DEFINED,
+            value: '',
+        },
+        valueList: [
+            {
+                id: `default_variable-${defaultVariable}-value`,
+                type: VARIABLE_DEFAULT_TYPE.DEFAULT_DEFINED,
+                value: '',
+            },
+        ],
+    };
+});
