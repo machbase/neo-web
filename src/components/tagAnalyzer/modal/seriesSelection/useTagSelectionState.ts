bc7a94eff35240cd30a5a93e6d000eb5750a3373
john kim <johnk.kim@machbase.com>
2026-06-02 16:28:33 +1000

    refactor

--- src/components/tagAnalyzer/modal/seriesSelection/useTagSelectionState.ts
+++ src/components/tagAnalyzer/modal/seriesSelection/useTagSelectionState.ts
@@ -380,7 +380,6 @@ export const useTagSelectionState = ({
         selectedTable,
         setSelectedTable: changeTable,
         tableOptions,
-        tableColumns,
         availableTags,
         tagTotal,
         tagInputValue,
